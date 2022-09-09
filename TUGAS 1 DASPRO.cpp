22aca771f658869f57f0b354ddd9c2a7b9c2375d
nitaalfi <112880617+nitaalfi@users.noreply.github.com>
2022-09-09 09:40:04 +0700

    Add files via upload

---
+++ TUGAS 1 DASPRO.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Everyone!" << endl;
+    cout << "NITA ALFIATUL JANNAH" << endl;
+    cout << "A11.2022.14643" << endl;
+    cout << "SMA N 3 BREBES" << endl;
+    return 0;
+}
