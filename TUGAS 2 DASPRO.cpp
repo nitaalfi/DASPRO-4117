535d9966546ec4a1509cd3647f82732fb82566d2
nitaalfi <112880617+nitaalfi@users.noreply.github.com>
2022-09-14 18:51:38 +0700

    Add files via upload

---
+++ TUGAS 2 DASPRO.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //integer
+    int x= 2000;
+    cout<<"Hello World!" <<x;
+    /*comment
+    */
+    int z;
+    z=10;
+
+    int u=18;
+    float t=155.5;
+    char b='M';
+    string ub="L";
+
+    cout<<"\nUmur saya:"<< u;
+    cout<<"\nTinggi saya:"<<t;
+    cout<<"\nUkuran baju:"<<b;
+    cout<<"\nUkuran celana:"<<ub;
+
+    return 0;
+}
