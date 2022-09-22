87308a5a904a6c57b22fa3e024343f32e3e5a580
nitaalfi <112880617+nitaalfi@users.noreply.github.com>
2022-09-22 19:45:32 +0700

    Add files via upload

---
+++ TUGAS 3 DASPRO.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NITA ALFIATUL JANNAH" << endl;
+    cout << "A11.2022.14643" << endl;
+
+    //Tugas menghitung luas lingkaran//
+
+    float p, r, hasil;
+    p= 3.14;
+
+    //r = 2, maka r kuadrat adalah 4//
+
+    r= 4;
+
+    cout << p * r << endl;
+    hasil= p * r;
+    cout << "hasil : " << hasil << endl;
+
+    return 0;
+}
