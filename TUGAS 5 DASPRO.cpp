7a0bde67c89418a743966ff538a7197ffe2adefe
nitaalfi <112880617+nitaalfi@users.noreply.github.com>
2022-10-09 18:15:10 +0700

    Add files via upload

---
+++ TUGAS 5 DASPRO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//TUGAS MENCARI KELIPATAN 5
+
+int x;
+x=0;
+do{
+    if (x % 5 == 0)
+    cout << x << endl;
+    x++;
+}
+while (x <= 50);
+    return 0;
+}
